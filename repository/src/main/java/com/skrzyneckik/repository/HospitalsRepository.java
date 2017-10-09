62db7afc3f96bd16497437597d00d7e45bc12930
Krzysztof Skrzynecki <krzysztof@digi.me>
2017-10-09 20:51:51 +0100

    Added http client to perform http request
    OkHttp is perfect candidate to handle http requests - it is a high quality mature library which has been adopted by Google as a default http client. For example purposes I've decided to use it to provide results in short time

--- repository/src/main/java/com/skrzyneckik/repository/HospitalsRepository.java
+++ repository/src/main/java/com/skrzyneckik/repository/HospitalsRepository.java
@@ -5,10 +5,16 @@ import com.skrzyneckik.domain.Hospital;
 import java.util.Arrays;
 import java.util.List;
 
+import okhttp3.OkHttpClient;
 import rx.Observable;
 
 public class HospitalsRepository {
 
+    private final OkHttpClient httpClient;
+
+    public HospitalsRepository() {
+        httpClient = new OkHttpClient.Builder().build();
+    }
     public Observable<List<Hospital>> hospitals() {
         return Observable.just(Arrays.asList(
                 new Hospital("Newquay Hospital"),
