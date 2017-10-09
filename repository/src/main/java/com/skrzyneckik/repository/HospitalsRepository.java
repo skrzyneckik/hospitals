43d998063bfbfd60402a203c0d53b051af1fc444
Krzysztof Skrzynecki <krzysztof@digi.me>
2017-10-09 20:52:50 +0100

    Created basic stream to download Hospital.csv file

--- repository/src/main/java/com/skrzyneckik/repository/HospitalsRepository.java
+++ repository/src/main/java/com/skrzyneckik/repository/HospitalsRepository.java
@@ -4,9 +4,13 @@ import com.skrzyneckik.domain.Hospital;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import rx.Observable;
+import rx.functions.Func1;
 
 public class HospitalsRepository {
 
@@ -15,10 +19,37 @@ public class HospitalsRepository {
     public HospitalsRepository() {
         httpClient = new OkHttpClient.Builder().build();
     }
+
     public Observable<List<Hospital>> hospitals() {
-        return Observable.just(Arrays.asList(
-                new Hospital("Newquay Hospital"),
-                new Hospital("Bridgewater Hospital")
-        ));
+        return Observable
+                .fromCallable(new Callable<Response>() {
+                    @Override
+                    public Response call() throws Exception {
+                        return httpClient.newCall(
+                                new Request.Builder().url("https://data.gov.uk/data/resource/nhschoices/Hospital.csv").build())
+                                .execute();
+                    }
+                })
+                .filter(new Func1<Response, Boolean>() {
+                    @Override
+                    public Boolean call(Response response) {
+                        return response.isSuccessful();
+                    }
+                })
+                .map(new Func1<Response, Object>() {
+                    @Override
+                    public Object call(Response response) {
+                        return null;
+                    }
+                })
+                .map(new Func1<Object, List<Hospital>>() {
+                    @Override
+                    public List<Hospital> call(Object ignored) {
+                        return Arrays.asList(
+                                new Hospital("Newquay Hospital"),
+                                new Hospital("Bridgewater Hospital")
+                        );
+                    }
+                });
     }
 }
