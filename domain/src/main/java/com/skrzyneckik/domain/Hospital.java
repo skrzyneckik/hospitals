f80aca970ac2bbfbc1a7fd268760005795a18253
Krzysztof Skrzynecki <krzysztof@digi.me>
2017-10-09 18:43:14 +0100

    Extended "Hospital" data class to store all of hospital's details

--- domain/src/main/java/com/skrzyneckik/domain/Hospital.java
+++ domain/src/main/java/com/skrzyneckik/domain/Hospital.java
@@ -4,4 +4,27 @@ package com.skrzyneckik.domain;
  * {@link Hospital} entity
  */
 public final class Hospital {
+
+    private int organisationID;
+    private String organisationCode;
+    private String organisationType;
+    private String subType;
+    private String sector;
+    private String organisationStatus;
+    private boolean isPimsManaged;
+    private String organisationName;
+    private String address1;
+    private String address2;
+    private String address3;
+    private String city;
+    private String county;
+    private String postcode;
+    private double latitude;
+    private double longitude;
+    private String parentODSCode;
+    private String parentName;
+    private String phone;
+    private String email;
+    private String website;
+    private String fax;
 }
